e831c6008b520365ba6d66cdb25624136936645d
aleccrowell <ba228jw@gmail.com>
2017-08-29 21:42:42 -0400

    fire

---
+++ build_index.sh
@@ -0,0 +1,5 @@
+wget ftp://ftp.ensembl.org/pub/current_fasta/mus_musculus/dna/Mus_musculus.*.dna.toplevel.fa.gz
+
+wget ftp://ftp.ensembl.org/pub/current_gff3/mus_musculus/Mus_musculus.GRCm38.89.gff3.gz
+
+
